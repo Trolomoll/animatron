a81771b2c39d43135b0a72316990544d02de2c89
Olusanmi Hundogan <o.hundogan@gmail.com>
2024-09-20 12:40:26 +0200

    Improvement?

--- effects/spooky_filter.py
+++ effects/spooky_filter.py
@@ -10,7 +10,7 @@ def spooky_filter_with_bpm(first_frame, bpm, last_beat_time, cap, screen_width, 
 
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    gray = cv2.GaussianBlur(gray, (15, 15), 1)
 
     current_time = time.time()
 
@@ -26,12 +26,12 @@ def spooky_filter_with_bpm(first_frame, bpm, last_beat_time, cap, screen_width, 
 
     delta_frame = cv2.absdiff(first_frame, gray)
     delta_frame_rgb = cv2.cvtColor(delta_frame, cv2.COLOR_GRAY2RGB)
-    random_color = np.array([random.randint(150, 255), random.randint(150, 255), random.randint(150, 255)], dtype=np.uint8)
+    random_color = np.array([random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)], dtype=np.uint8)
 
-    delta_frame_rgb_tinted = cv2.multiply(delta_frame_rgb, random_color)
+    delta_frame_rgb_tinted = cv2.multiply(delta_frame_rgb*0.1, random_color)
     delta_frame_rgb_tinted = np.clip(delta_frame_rgb_tinted, 0, 255)
     delta_frame_rgb_tinted = cv2.convertScaleAbs(delta_frame_rgb_tinted, alpha=0.6)
-    delta_frame_rgb_resized = cv2.resize(delta_frame_rgb_tinted, (screen_width, screen_height), interpolation=cv2.INTER_LINEAR)
+    delta_frame_rgb_resized = cv2.resize(delta_frame_rgb_tinted, (screen_width, screen_height), interpolation=cv2.INTER_CUBIC)
     delta_frame_rgb_swapped = np.swapaxes(delta_frame_rgb_resized, 0, 1)
 
     surface = pygame.surfarray.make_surface(delta_frame_rgb_swapped)
